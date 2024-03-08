b283a46089508cee994a3b00633995463ec042c4
Bảo Dương <duonghuybao17092002@gmail.com>
2024-03-08 09:50:15 +0700

    fix: hide file env

---
+++ controllers/tasks/updateTask.js
@@ -0,0 +1,44 @@
+const db = require('../../models/index')
+class UpdateTaskController {
+    async update(req, res) {
+        console.log(req.params);
+        try {
+            let id = req.params.id;
+            const task = await db.Task.findByPk(id);
+            if (!task) {
+                console.log('Không tìm thấy task');
+                return;
+            }
+            task.title = req.body.title;
+            task.description = req.body.description;
+            task.due_date = req.body.due_date;
+            task.priority = req.body.priority1;
+            task.completed = req.body.completed;
+            await task.save();
+            res.status(200).json('ok');
+        } catch (error) {
+            console.error('Lỗi khi xóa task:', error);
+        }
+    }
+    async updateTaskStatus(req, res) {
+        let id = req.params.id
+        console.log(req.body);
+        let status_id = req.body.status_id
+        let x = req.body.x
+        try {
+            const task = await db.Task.findByPk(id);
+            if (!task) {
+                console.log('Không tìm thấy task');
+                return;
+            }
+            task.status_id = status_id + x
+            console.log(status_id + x);
+            await task.save()
+            console.log('Task đã được xóa thành công');
+            res.status(200).json('ok');
+        } catch (error) {
+            console.error('Lỗi khi xóa task:', error);
+        }
+    }
+}
+module.exports = new UpdateTaskController;
--- routes/task.js
+++ routes/task.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var checkLogin = require('../middlewares/checkLogin')
 var viewListTask = require('../controllers/tasks/viewListTask')
 var createTask = require('../controllers/tasks/createTask')
+var updateTask = require('../controllers/tasks/updateTask')
 
 router.post('/create',checkLogin.check, createTask.create);
 router.get('/view', checkLogin.check, viewListTask.view)
+router.post('/update/status/:id',checkLogin.check, updateTask.updateTaskStatus);
 module.exports = router;
